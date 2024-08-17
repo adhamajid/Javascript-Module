68027d1eeb2b81af1d7c560126375b77c192fc0e
majid-hash-git <adha1900018252@webmail.uad.ac.id>
2024-08-17 19:01:34 +0700

    benarin structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const run = require("./app.js");
-const AlertService = require("./alert.service.js");
-const CalculatorService = require("./calculator.service.js");
-const JokesService = require("./jokes.service.js");
+const run = require("./app/app.js");
+const AlertService = require("./app/alert.service.js");
+const CalculatorService = require("./app/calculator.service.js");
+const JokesService = require("./app/jokes.service.js");
 
 const alertService = new AlertService();
 const calculatorService = new CalculatorService();
